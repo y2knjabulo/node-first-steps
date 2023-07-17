c684f04f8e35a340553a40dde7e469e9ed1112ba
y2knjabulo <y2knjabulo00gmail.com>
2023-07-17 14:37:19 +0200

    first commit

---
+++ greet.js
@@ -0,0 +1,8 @@
+function greet(name){
+    return "Hello, " + name; 
+    }
+    const msg = greet('Xola');
+    const msg1 = greet("njabulo");
+    console.log(msg);
+    console.log(msg1);
+    console.log(greet('Ntombi'));
